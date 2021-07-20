51c041ee1d2489f74fdba4239c1dd242fe251ff4
r-svp26 <riteshk6341@gmail.com>
2021-07-20 15:08:10 +0530

    [Ritesh] Add: Part time employee is added

--- EmployeeWage.cs
+++ EmployeeWage.cs
@@ -7,23 +7,29 @@ namespace EmployeeWageComputation
     class EmployeeWage
     {
         // constants
+        const int IS_PART_TIME = 1;
+        const int IS_FULL_TIME = 2;
         const int EMP_RATE_PER_HR = 20;
-        const int FULL_TIME_HR = 8;
+
         // variables
         int totalWage = 0;
+        int empHrs = 0;
 
         /// <summary>
-        /// This methed is used to evaluate the daily wage of an employee.
+        /// This method is used to evaluate the daily wage of a part time employee also.
         /// </summary>
         public void dailyWage() 
         {
             Random rd = new Random();
-            int empCheck = rd.Next(0, 2);
-            if (empCheck == 0)
-                totalWage = EMP_RATE_PER_HR * FULL_TIME_HR;
+            int empCheck = rd.Next(0, 3);
+            if (empCheck == 2)
+                empHrs = 8;
+            else if (empCheck == 1)
+                empHrs = 4;
             else
-                totalWage = 0;
-            Console.WriteLine("Employee daily wage is:" +totalWage);
+                empHrs = 0;
+            totalWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee daily wage:" +totalWage);
         }
     }
 }
